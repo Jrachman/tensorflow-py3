9411b0ab4e2e3b817957e3a63a7df71d61c2a2fe
Julian Rachman <jmrachman@gmail.com>
2017-07-15 01:05:41 -0700

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -19,7 +19,7 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python3'
-c.NotebookApp.iopub_data_rate_limit = 1000000
+c.NotebookApp.iopub_data_rate_limit = 100000000
 
 # sets a password if PASSWORD is set in the environment
 if 'PASSWORD' in os.environ:
